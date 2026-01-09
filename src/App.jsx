c167946fa5d13d8b3e662af303fe919e4d8b452f
hengthay <laovkimhengthai@gmail.com>
2026-01-09 19:36:26 +0700

    Manipulate path of url

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Project from './pages/Project'
+import Education from './pages/Education'
+import Blog from './pages/Blog'
+import Contact from './pages/Contact'
+import MainLayout from './components/layouts/MainLayout'
 
 const App = () => {
   return (
-    <div className='flex justify-center items-center mx-auto'>
-      <div className='mt-20'>
-        <p className='font-bold text-xl tracking-wide bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%'>Hello</p>
-      </div>
-    </div>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path='/' element={<Home />} index/>
+        <Route path='/about' element={<About />} />
+        <Route path='/portfolio' element={<Project />} />
+        <Route path='/resume' element={<Education />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/contact' element={<Contact />} />
+      </Route>
+    </Routes>
   )
 }
 
