169eb36c2477a5e53b175b0063f445138e9eab36
hengthay <laovkimhengthai@gmail.com>
2026-01-09 19:19:43 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center mx-auto'>
+      <div className='mt-20'>
+        <p className='font-bold text-xl tracking-wide bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%'>Hello</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
